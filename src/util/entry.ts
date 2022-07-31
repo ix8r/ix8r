9414d7c2987279f46d672b37068417e28ebcf90f
Kacper Seredyn <kacper.seredyn@tink.com>
2022-07-31 22:38:51 +0200

    Improved entry script

--- src/util/entry.ts
+++ src/util/entry.ts
@@ -8,7 +8,10 @@ export function defineEntry(name: string, entry: EntryFunction) {
 
 export function enter(name: string, argv: any) {
     if (!entryPoints.has(name)) {
-        console.log(`Unknown entry point ${name}`)
+        console.log(`Unknown entry point '${name}'\n`)
+
+        console.log("Known entry points:")
+        console.log([...entryPoints.keys()].map(s => `\t${s}`).join("\n"))
         return
     }
 
